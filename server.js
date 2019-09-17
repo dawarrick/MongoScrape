c57222bdcbc39ca65e6a876b2086087cd7ea59bd
Deb <49885983+dawarrick@users.noreply.github.com>
2019-09-17 12:52:08 -0400

    got multiple notes adding, still need to format out and input

--- server.js
+++ server.js
@@ -144,9 +144,10 @@ app.get("/", function (req, res) {
 app.get("/articles", function (req, res) {
   // Grab every document in the Articles collection. Sort by the most recent first
   db.Article.find({}).sort({ dateAdded: -1, _id: 1 })
+  .populate("note")
     .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      // res.json(dbArticle);
+      console.log(JSON.stringify(dbArticle));
       res.render("index", {
         articles: dbArticle
       });
@@ -179,12 +180,17 @@ app.get("/articles/:id", function (req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
+  console.log("req.param.id "+JSON.stringify(req.param.id))
+  console.log("body "+JSON.stringify(req.body))
   db.Note.create(req.body)
     .then(function (dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      console.log("req.param.id2 "+req.param.id)
+      console.log("note2 "+dbNote._id)
+      //return db.Library.findOneAndUpdate({}, { $push: { books: dbBook._id } }, { new: true });
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: {note: dbNote._id } }, { new: true });
     })
     .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
