ec14542846de51920c308a87632fd2d034ef1f1e
Deb <49885983+dawarrick@users.noreply.github.com>
2019-09-23 19:30:28 -0400

    go back

--- server.js
+++ server.js
@@ -46,12 +46,12 @@ app.use(express.static("public"));
 //mongoose.connect("mongodb://localhost/MongoScrape", { useNewUrlParser: true });
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/MongoScrape";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/MongoScrape";
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongoscrape:mongoscrape1@ds143767.mlab.com:43767/heroku_cttt7gwb";
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongoscrape:mongoscrape1@ds143767.mlab.com:43767/heroku_cttt7gwb";
 //mongodb://<dbuser>:<dbpassword>@ds143767.mlab.com:43767/heroku_cttt7gwb
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, { userMongoClient:true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, { useMongoClient:true });
 
 
 
